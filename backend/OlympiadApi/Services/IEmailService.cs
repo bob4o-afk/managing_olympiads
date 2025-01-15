5ea7f0e42b312eb59a406bc0cde3ec3b29c3d521
bob4o-afk <bobkatasad@abv.bg>
2025-01-15 15:36:39 +0200

    feat: adding api for sending documents

--- backend/OlympiadApi/Services/IEmailService.cs
+++ backend/OlympiadApi/Services/IEmailService.cs
@@ -3,5 +3,6 @@ namespace OlympiadApi.Services
     public interface IEmailService
     {
         Task SendEmailAsync(string toEmail, string subject, string body, string? ccEmail = null);
+        Task SendEmailWithAttachmentAsync(string toEmail, string subject, string body, IFormFile document, string? ccEmail = null);
     }
 }
